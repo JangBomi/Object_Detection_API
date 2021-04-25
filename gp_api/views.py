69990e4c50154ba621866639ee5e4ef2c3361432
bomi <bonita9931@gmail.com>
2021-04-25 18:43:09 +0900

    modify: token 방식 bearer 로 수정

--- gp_api/views.py
+++ gp_api/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets, permissions, status
 from rest_framework.response import Response
 from knox.models import AuthToken
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
 
 from .models import Post, Notes, User
 from .serializers import (
@@ -74,7 +77,8 @@ def login(request):
 
 
 @api_view(['POST', 'GET'])
-@permission_classes([AllowAny])
+@permission_classes((IsAuthenticated, ))
+@authentication_classes((JSONWebTokenAuthentication,))
 def record(request):
     if request.method == 'GET':
         serializer = RecordSerializer()
